5ba9cfb07e77f38f65993d02c0deec6f5c172f92
Kasif11 <kasif1196quamar@gmail.com>
2024-08-15 10:25:26 +0530

    Second Commit

---
+++ Calculator/script.js
@@ -0,0 +1,27 @@
+// Selects the input box by their Id
+let input = document.getElementById('inputBox');
+// Selecting all the buttons by query selector and storing them in the button variables
+let buttons = document.querySelectorAll('button')
+// Take Empty String for storing the result
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
